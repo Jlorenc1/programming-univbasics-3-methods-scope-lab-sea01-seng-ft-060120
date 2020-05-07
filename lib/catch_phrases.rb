175a09c3fdb1837ddd3c3a06524d32876a66ab93
Joshua Lorenc <joshua.lorenc@icloud.com>
2020-05-07 19:40:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,6 +9,9 @@ def toadstool
 end
 
 def link
-  phrase = "It's Dangerous To Go Alone! Take This."
-  puts phrase
+  warning = "It's Dangerous To Go Alone! Take This."
+  puts warning
 end
+
+def all_phrases
+  
